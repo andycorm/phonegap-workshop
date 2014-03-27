4bb5d10cd1aec3682a29b96ddbec75953bd43009
Andy Corman <andycorm@gmail.com>
2014-03-27 13:04:38 -0700

    added handlebars templating

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
 var app = {
 
     findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
+      var self = this;
+      this.store.findByName($('.search-key').val(), function(employees) {
+        $('.employee-list').html(self.employeeLiTpl(employees));
+      });
     },
 
     initialize: function() {
-        var self = this;
-        this.store = new MemoryStore(function(){
-          self.showAlert('Store Initialized', 'Info');
-        });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+      var self = this;
+      this.homeTpl = Handlebars.compile($("#home-tpl").html());
+      this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+      this.store = new MemoryStore(function() {
+        self.renderHomeView();
+      });
+    },
+
+    renderHomeView: function() {
+      $('body').html(this.homeTpl());
+      $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
 
     showAlert: function (message, title) {
